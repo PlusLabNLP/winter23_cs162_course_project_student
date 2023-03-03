22f3081845361fca1a4a44b64e34483a0e53513d
rohanbattula <rohanbattula@gmail.com>
2023-03-02 22:15:45 -0800

    added model training code, working on evaluation

--- trainers/train.py
+++ trainers/train.py
@@ -218,7 +218,10 @@ def train(args, train_dataset, model, tokenizer):
             # (2) Compute the loss (store as `loss` variable)
             # Hint: See the HuggingFace transformers doc to properly get
             # the loss from the model outputs.
-            raise NotImplementedError("Please finish the TODO!")
+            #raise NotImplementedError("Please finish the TODO!")
+
+            outputs = model(inputs)
+            loss = outputs[0]
 
             if args.n_gpu > 1:
                 # Applies mean() to average on multi-gpu parallel training.
@@ -230,7 +233,7 @@ def train(args, train_dataset, model, tokenizer):
                 loss = loss / args.gradient_accumulation_steps
 
             # (3) Implement the backward for loss propagation
-            raise NotImplementedError("Please finish the TODO!")
+            loss.backward()
 
             # End of TODO.
             ##################################################
@@ -376,7 +379,11 @@ def evaluate(args, model, tokenizer, prefix="", data_split="test"):
             ##################################################
             # TODO: Evaluation Loop
             # (1) Run forward and get the model outputs
-            raise NotImplementedError("Please finish the TODO!")
+            #raise NotImplementedError("Please finish the TODO!")
+            
+
+                outputs = model(inputs)
+
 
             if has_label or args.training_phase == "pretrain":
                 # (2) If label present or pretraining, compute the loss and prediction logits
@@ -386,11 +393,18 @@ def evaluate(args, model, tokenizer, prefix="", data_split="test"):
                 # indexing properly the outputs as tuples.
                 # Make sure to perform a `.mean()` on the eval loss and add it
                 # to the `eval_loss` variable.
-                raise NotImplementedError("Please finish the TODO!")
+                from torch.nn import functional as F
+                labels = torch.tensor([1,0]).unsqueeze(0)
+                logits = outputs[0]
+                loss = F.cross_entropy(labels, logits)
+                eval_loss = loss.mean()
+
+                #raise NotImplementedError("Please finish the TODO!")
             else:
                 # (3) If labels not present, only compute the prediction logits
                 # Label the logits as `logits`
-                raise NotImplementedError("Please finish the TODO!")
+                logits = outputs[0]
+                #raise NotImplementedError("Please finish the TODO!")
 
             # (4) Convert logits into probability distribution and relabel as `logits`
             # Hint: Refer to Softmax function
